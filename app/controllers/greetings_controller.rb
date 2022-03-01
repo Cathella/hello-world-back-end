f270a9df894a012977075147a926fd84df5b276f
Cathella <ncathie214@gmail.com>
2022-03-01 03:20:27 +0300

    Fix linters

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,6 +1,4 @@
 class GreetingsController < ApplicationController
-
-  # GET /greetings
   def index
     greeting = Greeting.order(Arel.sql('RANDOM()')).first
     render json: { message: greeting&.message }
